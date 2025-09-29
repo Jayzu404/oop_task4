b76fb82f2d8f4ddfa9d0348b49970d0f2412c029
teejay <tjayarancinaofficial@gmail.com>
2025-09-29 11:57:04 +0800

    feat: added Grilling class that implements CookingMethod interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+import 'kitchen_service.dart';
+import 'grilling.dart';
 void main(){
-  
+  print("\n\n=============== KITCHEN COOKING DEMO INTEGRATION ================\n");
+
+  print("===== IMPLEMENTATION B DEMO =====");
+  Grilling baking = Grilling();
+  KitchenService service2 = KitchenService(baking);
+
+  service2.prepareDish("Pork Belly");
+  service2.showCookingMethod();
+
+  print("\n");
 }

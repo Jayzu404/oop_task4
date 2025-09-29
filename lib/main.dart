397840c133e8699c8625fbf14c3d8c9b9b99ea29
teejay <tjayarancinaofficial@gmail.com>
2025-09-29 11:46:30 +0800

    feat: added Baking class that implements CookingMethod interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+import 'kitchen_service.dart';
+import 'baking.dart';
 void main(){
-  
+  print("\n\n=============== KITCHEN COOKING DEMO INTEGRATION ================\n");
+
+  print("===== IMPLEMENTATION A DEMO =====");
+  Baking baking = Baking();
+  KitchenService service1 = KitchenService(baking);
+
+  service1.prepareDish("Potato");
+  service1.showCookingMethod();
+
+  print("\n");
 }
